2876aaf25d2e388fa1d001ea15ac764f1d009f43
manisero <michalaniserowicz@gmail.com>
2018-08-12 23:38:15 +0200

    Started implementing telemetry

---
+++ src/Manisero.StreamProcessingModel.Samples/telemetry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Manisero.StreamProcessingModel.Samples
+{
+    public class telemetry
+    {
+        [Fact]
+        public void test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
