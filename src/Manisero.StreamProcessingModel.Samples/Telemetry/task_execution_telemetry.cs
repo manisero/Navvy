d7530d81b8d59a756c7013237ee3474cd6b6fdb0
manisero <michalaniserowicz@gmail.com>
2018-08-30 18:12:15 +0200

    Testing events handling

---
+++ src/Manisero.StreamProcessingModel.Samples/Telemetry/task_execution_telemetry.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using FluentAssertions;
+using Manisero.StreamProcessingModel.BasicProcessing;
+using Manisero.StreamProcessingModel.Core.Events;
+using Manisero.StreamProcessingModel.Core.Models;
+using Manisero.StreamProcessingModel.Samples.Utils;
+using Xunit;
+
+namespace Manisero.StreamProcessingModel.Samples.Telemetry
+{
+    public class task_execution_telemetry
+    {
+        [Fact]
+        public void task_start_and_end_is_reported()
+        {
+            // Arrange
+            TaskStartedEvent? startedEvent = null;
+            TaskEndedEvent? endedEvent = null;
+
+            var events = new TaskExecutionEvents();
+            events.TaskStarted += x => startedEvent = x;
+            events.TaskEnded += x => endedEvent = x;
+
+            var taskDescription = new TaskDescription
+            {
+                Steps = new List<ITaskStep>
+                {
+                    BasicTaskStep.Empty("Step")
+                }
+            };
+            
+            var progress = new Progress<TaskProgress>(_ => { });
+            var cancellationSource = new CancellationTokenSource();
+            var executor = TaskExecutorFactory.Create(ResolverType.Sequential, events);
+
+            // Act
+            executor.Execute(taskDescription, progress, cancellationSource.Token);
+
+            // Assert
+            startedEvent.Should().NotBeNull();
+            startedEvent.Value.Task.ShouldBeEquivalentTo(taskDescription);
+
+            endedEvent.Should().NotBeNull();
+            endedEvent.Value.Task.ShouldBeEquivalentTo(taskDescription);
+            endedEvent.Value.Duration.Ticks.Should().BePositive();
+        }
+
+        [Fact]
+        public void step_start_and_end_is_reported()
+        {
+            // Arrange
+            StepStartedEvent? startedEvent = null;
+            StepEndedEvent? endedEvent = null;
+
+            var events = new TaskExecutionEvents();
+            events.StepStarted += x => startedEvent = x;
+            events.StepEnded += x => endedEvent = x;
+
+            var taskDescription = new TaskDescription
+            {
+                Steps = new List<ITaskStep>
+                {
+                    BasicTaskStep.Empty("Step")
+                }
+            };
+
+            var progress = new Progress<TaskProgress>(_ => { });
+            var cancellationSource = new CancellationTokenSource();
+            var executor = TaskExecutorFactory.Create(ResolverType.Sequential, events);
+
+            // Act
+            executor.Execute(taskDescription, progress, cancellationSource.Token);
+
+            // Assert
+            startedEvent.Should().NotBeNull();
+            startedEvent.Value.Step.ShouldBeEquivalentTo(taskDescription.Steps[0]);
+
+            endedEvent.Should().NotBeNull();
+            endedEvent.Value.Step.ShouldBeEquivalentTo(taskDescription.Steps[0]);
+            endedEvent.Value.Duration.Ticks.Should().BePositive();
+        }
+    }
+}
