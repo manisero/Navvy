684f39285060af15818e313881b729b872190554
manisero <michalaniserowicz@gmail.com>
2018-08-31 07:19:39 +0200

    Added Benchmarks project

---
+++ src/Manisero.Navvy.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Manisero.Navvy.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<benchmark>();
+        }
+    }
+}
---
+++ src/Manisero.Navvy.Benchmarks/benchmark.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Manisero.Navvy.Benchmarks
+{
+    public class benchmark
+    {
+        [Benchmark(Baseline = true)]
+        public void option1()
+        {
+        }
+
+        [Benchmark]
+        public void option2()
+        {
+        }
+    }
+}
