fd0699aed60a29209eec41b71c33ec863a62a070
manisero <michalaniserowicz@gmail.com>
2018-09-02 09:50:02 +0200

    Increased version

--- src/Manisero.Navvy.Dataflow/Properties/AssemblyInfo.cs
+++ src/Manisero.Navvy.Dataflow/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("69123f93-4ca6-4a7f-8df6-5325783722c6")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
