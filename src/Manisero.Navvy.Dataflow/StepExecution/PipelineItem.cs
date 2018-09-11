3ea8c7b800c4183911759b59ffe9ae88496dcd81
manisero <michalaniserowicz@gmail.com>
2018-09-11 20:28:20 +0200

    Recreated Dataflow project

---
+++ src/Manisero.Navvy.Dataflow/StepExecution/PipelineItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Manisero.Navvy.Dataflow.StepExecution
+{
+    internal class PipelineItem<TItem>
+    {
+        public int Number { get; set; }
+
+        public TItem Item { get; set; }
+
+        public Stopwatch ProcessingStopwatch { get; set; }
+    }
+}
