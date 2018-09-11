ef539c875df46c437577dff3ec05a2347dfd117a
manisero <michalaniserowicz@gmail.com>
2018-09-11 20:52:18 +0200

    Recreating Benchmarks project

---
+++ src/Manisero.Navvy.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manisero.Navvy.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
