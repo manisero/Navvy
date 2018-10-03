141d89e2153b946682d17b1da326b0a8301d5fcc
manisero <michalaniserowicz@gmail.com>
2018-10-03 23:13:47 +0200

    Added IPipelineTaskStep interface

--- src/Manisero.Navvy/PipelineProcessing/PipelineTaskStep.cs
+++ src/Manisero.Navvy/PipelineProcessing/PipelineTaskStep.cs
@@ -4,6 +4,11 @@ using Manisero.Navvy.PipelineProcessing.Models;
 
 namespace Manisero.Navvy.PipelineProcessing
 {
+    public interface IPipelineTaskStep : ITaskStep
+    {
+        IReadOnlyCollection<IPipelineBlock> Blocks { get; }
+    }
+
     public static class PipelineTaskStep
     {
         public static PipelineTaskStepBuilder<TItem> Builder<TItem>(
@@ -26,20 +31,22 @@ namespace Manisero.Navvy.PipelineProcessing
         }
     }
 
-    public class PipelineTaskStep<TItem> : ITaskStep
+    public class PipelineTaskStep<TItem> : IPipelineTaskStep
     {
         public string Name { get; }
 
         public Func<TaskOutcome, bool> ExecutionCondition { get; }
 
-        public IPipelineInput<TItem> Input { get; set; }
+        public IPipelineInput<TItem> Input { get; }
 
-        public IList<PipelineBlock<TItem>> Blocks { get; }
+        public IReadOnlyList<PipelineBlock<TItem>> Blocks { get; }
+
+        IReadOnlyCollection<IPipelineBlock> IPipelineTaskStep.Blocks => Blocks;
 
         public PipelineTaskStep(
             string name,
             IPipelineInput<TItem> input,
-            IList<PipelineBlock<TItem>> blocks,
+            IReadOnlyList<PipelineBlock<TItem>> blocks,
             Func<TaskOutcome, bool> executionCondition = null)
         {
             Name = name;
@@ -52,7 +59,7 @@ namespace Manisero.Navvy.PipelineProcessing
             string name,
             IEnumerable<TItem> input,
             int expectedItemsCount,
-            IList<PipelineBlock<TItem>> blocks,
+            IReadOnlyList<PipelineBlock<TItem>> blocks,
             Func<TaskOutcome, bool> executionCondition = null)
             : this(name, new PipelineInput<TItem>(input, expectedItemsCount), blocks, executionCondition)
         {
@@ -61,7 +68,7 @@ namespace Manisero.Navvy.PipelineProcessing
         public PipelineTaskStep(
             string name,
             ICollection<TItem> input,
-            IList<PipelineBlock<TItem>> blocks,
+            IReadOnlyList<PipelineBlock<TItem>> blocks,
             Func<TaskOutcome, bool> executionCondition = null)
             : this(name, input, input.Count, blocks, executionCondition)
         {
