92296332c5c5b0d8312b3b228a5404f187cdbe37
manisero <michalaniserowicz@gmail.com>
2019-04-15 22:23:06 +0200

    BatchedPipelineInputItems now accepts batchFactory

--- src/Manisero.Navvy/Utils/BatchedPipelineInputItems.cs
+++ src/Manisero.Navvy/Utils/BatchedPipelineInputItems.cs
@@ -1,21 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Manisero.Navvy.PipelineProcessing.Models;
 
 namespace Manisero.Navvy.Utils
 {
-    public class BatchedPipelineInputItems<TItem> : IPipelineInputItems<ICollection<TItem>>
+    public class BatchedPipelineInputItems<TItem, TBatch> : IPipelineInputItems<TBatch>
     {
-        public IEnumerable<ICollection<TItem>> Items { get; }
+        public IEnumerable<TBatch> Items { get; }
 
         public int ExpectedCount { get; }
 
         public BatchedPipelineInputItems(
             IEnumerable<TItem> items,
             int expectedItemsCount,
-            int batchSize)
+            int batchSize,
+            Func<ICollection<TItem>, TBatch> batchFactory)
         {
-            Items = items.Batch(batchSize);
+            Items = items.Batch(batchSize).Select(batchFactory);
             ExpectedCount = expectedItemsCount.CeilingOfDivisionBy(batchSize);
+        }
+
+        public BatchedPipelineInputItems(
+            ICollection<TItem> items,
+            int batchSize,
+            Func<ICollection<TItem>, TBatch> batchFactory)
+            : this(items, items.Count, batchSize, batchFactory)
+        {
+        }
+    }
+
+    public class BatchedPipelineInputItems<TItem> : BatchedPipelineInputItems<TItem, ICollection<TItem>>
+    {
+        public BatchedPipelineInputItems(
+            IEnumerable<TItem> items,
+            int expectedItemsCount,
+            int batchSize) 
+            : base(items, expectedItemsCount, batchSize, x => x)
+        {
         }
 
         public BatchedPipelineInputItems(
